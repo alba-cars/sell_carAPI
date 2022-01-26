dc9634d65cfdbd68cc862dc4197cbcbf1d5fc553
albadorbor <dorbor@albacars.ae>
2022-01-26 13:42:10 +0400

    new update

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HomeSellDrop from './components/home'
 import Login from './components/login'
-import LogDemo from './components/logdemo'
 import Users from './components/home/users/index'
 import CreateUser from './components/home/users/create'
 import { Routes, Route } from "react-router-dom"
@@ -13,7 +12,6 @@ function App() {
         <Routes path='/'>
           <Route index element={<HomeSellDrop />} />
           <Route path="login" element={<Login />} />
-          <Route path="logdemo" element={<LogDemo />} />
           <Route path="users" element={<Users />} />
           <Route path="/users/create" element={<CreateUser />} />
         </Routes>
