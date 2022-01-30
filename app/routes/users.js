59367b79fdd453c0a5ef3b532a46892eaefe9645
amitha <amitha@amithas-iMac.local>
2022-01-30 12:47:57 +0400

    added user update api

--- app/routes/users.js
+++ app/routes/users.js
@@ -66,6 +66,29 @@ router.get("/:id",  (req, res) => {
         res.status(400).json({ error: err })
     })  
   });
+router.put('/:id', async (req, res)  => {
+
+User.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    email: req.body.email,
+    roles: req.body.roles,
+    status:req.body.status
+}, {new: true})
+.then(user => {
+    if(!user) {
+        return res.status(404).send({
+            message: "User not found with id " + req.params.id
+        });
+    }
+    res.send(user);
+}).catch(err => {
+    return res.status(500).send({
+        message: "Error updating user with id " + req.params.id
+    });
+});
+})
+
+  
 
 
 module.exports = router;
